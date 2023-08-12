0c5bd1a15ddef034f0146702e8331e8c7a6743c5
Dean Gawler <58622132+dgawler@users.noreply.github.com>
2023-08-12 10:18:13 +0930

    Add files via upload

---
+++ pico_wifi_config.py
@@ -0,0 +1,8 @@
+# Wifi parameters for the Pico to use to connect to your network
+# Change them to suit your wifi network, and the server on which
+# the PicoWeatherServer.py program will be running
+#
+host = '192.168.1.33'
+port = 5000
+ssid = 'my_network'
+ssid_password = 'abcd1234'
